11bbdf00967ca9daa4258ecf37f471834e9155b2
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-26 14:14:30 -0500

    creacion barra de busqueda

--- src/app/mapas/mapas.module.ts
+++ src/app/mapas/mapas.module.ts
@@ -5,9 +5,11 @@ import { MapViewComponent } from './components/map-view/map-view.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { BtnMyLocationComponent } from './components/btn-my-location/btn-my-location.component';
 import { AngularLogoComponent } from './components/angular-logo/angular-logo.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 @NgModule({
-  declarations: [MapScreenComponent, MapViewComponent, LoadingComponent, BtnMyLocationComponent, AngularLogoComponent],
+  declarations: [MapScreenComponent, MapViewComponent, LoadingComponent, BtnMyLocationComponent, AngularLogoComponent, SearchBarComponent, SearchResultsComponent],
   imports: [CommonModule],
   exports: [MapScreenComponent],
 })
