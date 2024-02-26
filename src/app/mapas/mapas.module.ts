4e05d91a2b1527cb24b50c0f2d2d931a42101598
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-25 22:30:11 -0500

    creacion de modulo de mapas

---
+++ src/app/mapas/mapas.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapScreenComponent } from './screens/map-screen/map-screen.component';
+
+@NgModule({
+  declarations: [MapScreenComponent],
+  imports: [CommonModule],
+  exports: [MapScreenComponent],
+})
+export class MapasModule {}
---
+++ src/app/mapas/screens/map-screen/map-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-screen',
+  templateUrl: './map-screen.component.html',
+  styleUrl: './map-screen.component.css'
+})
+export class MapScreenComponent {
+
+}
