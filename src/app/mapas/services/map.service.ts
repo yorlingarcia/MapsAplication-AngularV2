7100f974f03615dc1940062970989758dc13e222
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-27 07:42:19 -0500

    creacion de marcadores y su respectivo popup

--- src/app/mapas/services/map.service.ts
+++ src/app/mapas/services/map.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map } from 'mapbox-gl';
+import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { Feature } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MapService {
   private map?: Map | undefined;
+  private markers: Marker[] = [];
 
   constructor() {}
 
@@ -23,6 +25,35 @@ export class MapService {
     this.map?.flyTo({
       zoom: 14,
       center: coords,
+    });
+  }
+
+  createMarkersFromPlaces(places: Feature[], userLocation: [number, number]) {
+    if (!this.map) throw Error(`Mapa no inicializado`);
+
+    this.markers.forEach((marker) => marker.remove());
+    const newMarkers = [];
+    for (const place of places) {
+      const [lng, lat] = place.center;
+      const popup = new Popup().setHTML(`
+      <h6>${place.text}</h6>
+      <span>${place.place_name}</span>
+      `);
+      const newMarker = new Marker()
+        .setLngLat([lng, lat])
+        .setPopup(popup)
+        .addTo(this.map);
+      newMarkers.push(newMarker);
+    }
+    this.markers = newMarkers;
+
+    if (places.length === 0) return;
+
+    const bounds = new LngLatBounds();
+    newMarkers.forEach((marker) => bounds.extend(marker.getLngLat()));
+    bounds.extend(userLocation);
+    this.map.fitBounds(bounds, {
+      padding: 200,
     });
   }
 }
--- src/app/mapas/services/places.service.ts
+++ src/app/mapas/services/places.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Feature, PlacesResponse } from '../interfaces';
 import { PlacesApiClient } from '../api';
+import { MapService } from './map.service';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +16,10 @@ export class PlacesService {
     return !!this.useLocation;
   }
 
-  constructor(private placesApi: PlacesApiClient) {
+  constructor(
+    private placesApi: PlacesApiClient,
+    private mapService: MapService
+  ) {
     this.getUserLocation();
   }
 
@@ -50,10 +54,9 @@ export class PlacesService {
         },
       })
       .subscribe((resp) => {
-        console.log(resp.features);
-
         this.isLoadingPlaces = false;
         this.places = resp.features;
+        this.mapService.createMarkersFromPlaces(this.places, this.useLocation!);
       });
   }
 }
